5e76bcc00ac05def98bf30e1bdfed05fe045bbf5
Imani Manyara <imani@simtabi.com>
2021-03-19 14:54:13 -0400

    Some refactors

--- src/Base/Support/Traits/SupportHelpersTrait.php
+++ src/Base/Support/Traits/SupportHelpersTrait.php
@@ -7,6 +7,9 @@ trait SupportHelpersTrait
 
     public function quickAccess($key, $data)
     {
+        if (empty($key)) {
+            return null;
+        }
         return $this->pheg->getFromArray($key, $data);
     }
 
