485086d42967c28e48b39a3b19fee3c96ff7491a
Imani Manyara <imani@simtabi.com>
2021-04-29 13:05:08 -0400

    Some refactors

--- src/Phegs/Navigation/Breadcrumbs.php
+++ src/Phegs/Navigation/Breadcrumbs.php
@@ -4,14 +4,42 @@ namespace Simtabi\Pheg\Phegs\Navigation;
 
 class Breadcrumbs
 {
+
     /**
      * @var
      */
     private $base;
+
     /**
      * @var null|string
      */
     private ?string $separator;
+
+    /**
+     * @var null|string
+     */
+    private ?string $ulClass;
+
+    /**
+     * @var null|string
+     */
+    private ?string $ulAriaLabel;
+
+    /**
+     * @var null|string
+     */
+    private ?string $liClass;
+
+    /**
+     * @var null|string
+     */
+    private ?string $liActiveClass = 'active';
+
+    /**
+     * @var null|string
+     */
+    private ?string $liAriaCurrent = 'page';
+
     /**
      * @var array
      */
@@ -23,9 +51,147 @@ class Breadcrumbs
      */
     public function __construct(?string $separator = null)
     {
-        $this->separator = $separator;
-        $this->links     = [];
+
+        $this->separator     = $separator;
+        $this->ulClass       = null;
+        $this->ulAriaLabel   = null;
+        $this->liClass       = null;
+        $this->liActiveClass = 'active';
+        $this->liAriaCurrent = 'page';
+        $this->links         = [];
+
     }
+
+    /**
+     * @return string|null
+     */
+    public function getSeparator(): ?string
+    {
+        return $this->separator;
+    }
+
+    /**
+     * @param string|null $separator
+     * @return Self
+     */
+    public function setSeparator(?string $separator): Self
+    {
+        $this->separator = $separator;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUlClass(): ?string
+    {
+        return $this->ulClass;
+    }
+
+    /**
+     * @param string|null $ulClass
+     * @return self
+     */
+    public function setUlClass(?string $ulClass): self
+    {
+        $this->ulClass = $ulClass;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUlAriaLabel(): ?string
+    {
+        return $this->ulAriaLabel;
+    }
+
+    /**
+     * @param string|null $ulAriaLabel
+     * @return self
+     */
+    public function setUlAriaLabel(?string $ulAriaLabel): self
+    {
+        $this->ulAriaLabel = $ulAriaLabel;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLiClass(): ?string
+    {
+        return $this->liClass;
+    }
+
+    /**
+     * @param string|null $liClass
+     * @return self
+     */
+    public function setLiClass(?string $liClass): self
+    {
+        $this->liClass = $liClass;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLiActiveClass(): ?string
+    {
+        return $this->liActiveClass;
+    }
+
+    /**
+     * @param string|null $liActiveClass
+     * @return self
+     */
+    public function setLiActiveClass(?string $liActiveClass): self
+    {
+        $this->liActiveClass = $liActiveClass;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLiAriaCurrent(): ?string
+    {
+        return $this->liAriaCurrent;
+    }
+
+    /**
+     * @param string|null $liAriaCurrent
+     * @return self
+     */
+    public function setLiAriaCurrent(?string $liAriaCurrent): self
+    {
+        $this->liAriaCurrent = $liAriaCurrent;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getLinks(): array
+    {
+        return $this->links;
+    }
+
+    /**
+     * @param array $links
+     * @return self
+     */
+    public function setLinks(array $links): self
+    {
+        $this->links = $links;
+        return $this;
+    }
+
+
+
+
+
 
     /**
      * @param string $baseUrl
@@ -41,7 +207,7 @@ class Breadcrumbs
         bool $showTitle = true,
         ?string $icon   = null,
         string $class   = null
-    ): Breadcrumbs
+    ): self
     {
         $this->base = [
             "url"       => $baseUrl,
@@ -57,9 +223,9 @@ class Breadcrumbs
      * @param string $title
      * @param string|null $url
      * @param null|string $class
-     * @return Breadcrumbs
+     * @return self
      */
-    public function addCrumb(string $title, ?string $url, ?string $class = null): Breadcrumbs
+    public function addCrumb(string $title, ?string $url, ?string $class = null): self
     {
         $this->links[] = $this->parts($title, $url, $class);
         return $this;
@@ -70,8 +236,10 @@ class Breadcrumbs
      */
     public function render(): string
     {
-        $init = "<nav aria-label=\"breadcrumb\"><ol class=\"breadcrumb\">";
-        $end  = "</ol></nav>";
+        $ulAriaLabel = $this->ulAriaLabel;
+        $ulClass     = $this->ulClass;
+        $init        = "<ul class=\"$ulClass\" aria-label=\"$ulAriaLabel\">";
+        $end         = "</ul>";
 
         return $init . ($this->links ? $this->setBase() : "") . $this->mount($this->links) . $end;
     }
@@ -90,15 +258,19 @@ class Breadcrumbs
         if (!$links) {
             return null;
         }
-        $last       = count($links) - 1;
-        $breadcrumb = "";
+
+        $last          = count($links) - 1;
+        $breadcrumb    = "";
+        $liClass       = $this->liClass;
+        $liActiveClass = $this->liActiveClass;
+        $liAriaCurrent = $this->liAriaCurrent;
 
         for ($b = 0; $b <= $last; $b++) {
 
             if ($b == $last) {
-                $breadcrumb .= "<li class=\"breadcrumb-item active {$links[$b]["class"]}\" aria-current=\"page\">{$this->separator}{$links[$b]["title"]}</li>" . "\n";
+                $breadcrumb .= "<li class=\"$liClass $liActiveClass {$links[$b]["class"]}\" aria-current=\"$liAriaCurrent\">{$this->separator}{$links[$b]["title"]}</li>" . "\n";
             } else {
-                $breadcrumb .= "<li class=\"breadcrumb-item {$links[$b]["class"]}\">{$this->separator}<a href=\"{$links[$b]["url"]}\">{$links[$b]["title"]}</a></li>" . "\n";
+                $breadcrumb .= "<li class=\"$liClass {$links[$b]["class"]}\">{$this->separator}<a href=\"{$links[$b]["url"]}\">{$links[$b]["title"]}</a></li>" . "\n";
             }
         }
 
@@ -110,16 +282,19 @@ class Breadcrumbs
      */
     private function setBase(): string
     {
-        $title = ($this->base["showTitle"] ? $this->base["title"] : null);
-        $icon = $this->base["icon"];
-        $class = $this->base["class"];
-        $url = $this->base["url"];
+        $title         = ($this->base["showTitle"] ? $this->base["title"] : null);
+        $icon          = $this->base["icon"];
+        $class         = $this->base["class"];
+        $url           = $this->base["url"];
+        $liClass       = $this->liClass;
+        $liActiveClass = $this->liActiveClass;
+        $liAriaCurrent = $this->liAriaCurrent;
 
         if (!$this->links) {
-            return "<li class=\"breadcrumb-item active {$class}\" aria-current=\"page\">{$icon}{$title}</li>";
+            return "<li class=\"$liClass $liActiveClass {$class}\" aria-current=\"$liAriaCurrent\">{$icon}{$title}</li>";
         }
 
-        return "<li class=\"breadcrumb-item {$class}\"><a href=\"{$url}\">{$icon}{$title}</a></li>";
+        return "<li class=\"$liClass {$class}\"><a href=\"{$url}\">{$icon}{$title}</a></li>";
     }
 
     /**
@@ -130,7 +305,7 @@ class Breadcrumbs
      */
     private function parts(
         string $title,
-        string $url = null,
+        string $url   = null,
         string $class = null
     ): array
     {
