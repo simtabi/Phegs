b2b50c98c1f7cf2933a112dcae19010226aff455
Imani Manyara <imani@simtabi.com>
2021-06-09 20:57:42 -0400

    Updates and fixes

--- src/Phegs/Helpers/Traits/StringToolsTrait.php
+++ src/Phegs/Helpers/Traits/StringToolsTrait.php
@@ -587,4 +587,18 @@ trait StringToolsTrait
         return $output;
     }
 
+
+
+    public static function fromCamelCase($string) {
+        $pattern = '!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!';
+        preg_match_all($pattern, $string, $matches);
+        $ret = $matches[0];
+        foreach ($ret as &$match) {
+            $match = $match == strtoupper($match) ?
+                strtolower($match) :
+                lcfirst($match);
+        }
+        return implode('_', $ret);
+    }
+
 }
