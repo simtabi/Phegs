aa7c78347124d17d98b4126f87568bfec3d0b66c
Imani Manyara <imani@simtabi.com>
2021-06-17 12:25:57 -0400

    Updates and fixes

--- src/Phegs/Ensue/Traits/MathValidatorsTrait.php
+++ src/Phegs/Ensue/Traits/MathValidatorsTrait.php
@@ -9,4 +9,10 @@ trait MathValidatorsTrait
         return ($number % 2 == 0) ? true : false;
     }
 
+    public static function roundOffToNearest ( $value, $precision = 2 ): float|int
+    {
+        $pow = pow ( 10, $precision );
+        return ( ceil ( $pow * $value ) + ceil ( $pow * $value - ceil ( $pow * $value ) ) ) / $pow;
+    }
+
 }
