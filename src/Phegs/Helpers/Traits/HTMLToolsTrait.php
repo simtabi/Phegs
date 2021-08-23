fa823280a85682596f9c5a16b39d769c1f264839
Imani Manyara <imani@simtabi.com>
2021-08-23 09:47:22 -0400

    Bug fixes

--- src/Phegs/Helpers/Traits/HTMLToolsTrait.php
+++ src/Phegs/Helpers/Traits/HTMLToolsTrait.php
@@ -5,6 +5,8 @@ namespace Simtabi\Pheg\Phegs\Helpers\Traits;
 use Simtabi\Pheg\Pheg;
 use Simtabi\Pheg\Phegs\Helpers\Components\HtmlTools\Html2Text;
 use Simtabi\Pheg\Phegs\Ensue\Ensue;
+use DOMDocument;
+use DOMXPath;
 
 trait HTMLToolsTrait
 {
@@ -78,4 +80,101 @@ trait HTMLToolsTrait
         return sprintf("%s%%[%s>%s]%s\r", $percentage, str_repeat("=", $bar), str_repeat(" ", $width-$bar), $info);
     }
 
+
+
+    public function getSvgIcon($path, $iconClass = "", $svgClass = "")
+    {
+
+        if ( ! file_exists($path)) {
+            return "<!-- SVG file not found: ".$path." -->";
+        }
+
+        $svg_content = file_get_contents($path);
+
+        $dom = new DOMDocument();
+        $dom->loadXML($svg_content);
+
+        // remove unwanted comments
+        $xpath = new DOMXPath($dom);
+        foreach ($xpath->query("//comment()") as $comment) {
+            $comment->parentNode->removeChild($comment);
+        }
+
+        // add class to svg
+        if ( ! empty($svgClass)) {
+            foreach ($dom->getElementsByTagName("svg") as $element) {
+                $element->setAttribute("class", $svgClass);
+            }
+        }
+
+        // remove unwanted tags
+        $title = $dom->getElementsByTagName("title");
+        if ($title["length"]) {
+            $dom->documentElement->removeChild($title[0]);
+        }
+
+        $desc = $dom->getElementsByTagName("desc");
+        if ($desc["length"]) {
+            $dom->documentElement->removeChild($desc[0]);
+        }
+
+        $defs = $dom->getElementsByTagName("defs");
+        if ($defs["length"]) {
+            $dom->documentElement->removeChild($defs[0]);
+        }
+
+        // remove unwanted id attribute in g tag
+        $g =  $dom->getElementsByTagName("g");
+        foreach ($g as $el) {
+            $el->removeAttribute("id");
+        }
+
+        $mask =  $dom->getElementsByTagName("mask");
+        foreach ($mask as $el) {
+            $el->removeAttribute("id");
+        }
+
+        $rect =  $dom->getElementsByTagName("rect");
+        foreach ($rect as $el) {
+            $el->removeAttribute("id");
+        }
+
+        $path =  $dom->getElementsByTagName("path");
+        foreach ($path as $el) {
+            $el->removeAttribute("id");
+        }
+
+        $circle =  $dom->getElementsByTagName("circle");
+        foreach ($circle as $el) {
+            $el->removeAttribute("id");
+        }
+
+        $use =  $dom->getElementsByTagName("use");
+        foreach ($use as $el) {
+            $el->removeAttribute("id");
+        }
+
+        $polygon =  $dom->getElementsByTagName("polygon");
+        foreach ($polygon as $el) {
+            $el->removeAttribute("id");
+        }
+
+        $ellipse =  $dom->getElementsByTagName("ellipse");
+        foreach ($ellipse as $el) {
+            $el->removeAttribute("id");
+        }
+
+        $string = $dom->saveXML($dom->documentElement);
+
+        // remove empty lines
+        $string = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $string);
+
+        $cls = ["svg-icon"];
+
+        if ( ! empty($iconClass)) {
+            $cls = array_merge($cls, explode(" ", $iconClass));
+        }
+
+        return "<span class=". implode(" ", $cls) .">" . $string . "</span>";
+    }
 }
